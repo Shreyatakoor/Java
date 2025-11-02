8150b1d3e92ae0cda84d16a4e4bbe5fcda4888d1
Shreyatakoor <shreyatakoor12@gmail.com>
2025-11-02 21:00:17 +0530

    Add Main3 class with static and instance variables

---
+++ Static.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    static int a=10;
+    int b=20;
+    public static void main(String[] args){
+        Main3 obj=new Main3();
+        System.out.println(a);
+        System.out.println(obj.b);
+    }
+    
+}
