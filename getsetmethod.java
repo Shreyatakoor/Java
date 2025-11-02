2bfa1e46d856c502b78991a105d08b64396e8027
Shreyatakoor <shreyatakoor12@gmail.com>
2025-11-02 19:04:41 +0530

    Implement Person class with age getter and setter

---
+++ getsetmethod.java
@@ -0,0 +1,21 @@
+public class Person {
+    private int age;
+    public int getAge(){
+        return age;
+     } 
+
+    public void setAge(int age){
+        if(age>=0){
+            this.age=age;
+            System.out.println(age);
+            
+        }else{
+             System.out.println("Invalid age");
+        }
+    }
+     public static void main(String[] args){
+         Person p1=new Person();
+         p1.getAge();
+         p1.setAge(10);
+     }
+}
