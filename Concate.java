7a7ffcf1940b4a9ca9405fc8f249e6f89f464a5d
Shreyatakoor <shreyatakoor12@gmail.com>
2025-11-02 19:10:31 +0530

    Add String concatenation example in Concate.java

---
+++ Concate.java
@@ -0,0 +1,8 @@
+public class StringConcate {
+    public static void main(String args[]){
+        String str1="Hello";
+        String str2= "Hema";
+        System.out.println(str1.concat(str2));
+    
+}
+}
