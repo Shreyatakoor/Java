2b31449ec401d2948b6ebef17e48f2abe2e8530c
Shreyatakoor <shreyatakoor12@gmail.com>
2025-11-02 19:28:33 +0530

    Add Zero class to check number sign

---
+++ Zero.java
@@ -0,0 +1,15 @@
+public class Zero {
+    public static void main(String args[]){
+        int number=0;
+        if(number>0){
+            System.out.println("positive number.");
+            
+        }else if(number<0)
+            System.out.println("Negative number.");
+        else{
+            System.out.println("Zero");
+        }
+            
+    }
+    
+}
