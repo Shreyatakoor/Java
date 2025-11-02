6c2faef751c7f13809c1aadaece27b6314c341c9
Shreyatakoor <shreyatakoor12@gmail.com>
2025-11-02 19:02:22 +0530

    Add Student class with display method

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    String name;
+    Student(){
+        name="John";
+    }
+    void display(){
+        System.out.println("Student name:" + name);
+        
+    }
+    public static void main(String[] args){
+        Student s1=new Student();
+        s1.display();
+    }
+    
+}
