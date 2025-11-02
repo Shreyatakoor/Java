54579ece88427f2dbfbacc36bf3642ec3d6b3f0f
Shreyatakoor <shreyatakoor12@gmail.com>
2025-11-02 19:24:38 +0530

    Add PositiveOddNumbers.java to check number parity

---
+++ PositiveOddNumbers.java
@@ -0,0 +1,17 @@
+public class Demo3 {
+    public static void main(String args[]){
+        int number=21;
+        if(number>0){
+            if(number%2==0)
+        
+            System.out.println("positive even number.");
+            else{
+               
+            System.out.println("positive odd number.");
+            }
+        }
+    }
+            
+}
+            
+    
