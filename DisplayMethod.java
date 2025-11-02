d1ce9b9f3505c7f00ec68a8e566ef63d622abe44
Shreyatakoor <shreyatakoor12@gmail.com>
2025-11-02 21:06:08 +0530

    Add NewClass5 with display method and main

---
+++ DisplayMethod.java
@@ -0,0 +1,15 @@
+public class NewClass5 {
+    int x=30;
+    static int y=40;
+    public void display(){
+        int r=x+y;
+        System.out.println(r);
+        
+    
+}
+    public static void main(String[] args){
+        NewClass5 obj=new NewClass5();
+        obj.display();
+    }
+    
+}
